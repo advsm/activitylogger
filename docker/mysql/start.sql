05b16bf23593456a95b8b84347f0c14fbcd36ef5
Andrey Smirnov <s.andrey.d@gmail.com>
2021-12-25 16:44:07 +0300

    Initialize project architecture via docker-compose

---
+++ docker/mysql/start.sql
@@ -0,0 +1,15 @@
+create database `activity`;
+use activity;
+
+create table `visit` (
+    `id` int(11) not null auto_increment,
+    `domain` varchar(64) not null,
+    `url` varchar(512) collate utf8_unicode_ci not null,
+    `ip` varchar(16) collate utf8_unicode_ci not null,
+    `user_agent` varchar(512) not null,
+    `created_at` datetime not null,
+    `updated_at` timestamp not null on update current_timestamp,
+    index (`domain`),
+    index (`url`),
+    primary key (`id`)
+) engine=InnoDB default charset=utf8 collate=utf8_unicode_ci;
